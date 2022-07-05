e9627de0fdf5ba5deeb962839f2b56e629852791
Stefan Chen <zchen158@my.bcit.ca>
2022-07-04 20:40:32 -0700

    Create browser history

--- react_material/src/index.js
+++ react_material/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const history = createBrowserHistory();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
