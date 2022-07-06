b7686bb43b9e058603530c7eee9f38fa37c81a08
Stefan Chen <zchen158@my.bcit.ca>
2022-07-06 10:45:35 -0700

    Add grid and card layout

--- react_material/src/components/Pokedex.js
+++ react_material/src/components/Pokedex.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>Hi</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 function Pokedex() {
+  const classes = useStyles();
   return (
     <>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar />
       </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
     </>
-  )
+  );
 }
 
 export default Pokedex;
