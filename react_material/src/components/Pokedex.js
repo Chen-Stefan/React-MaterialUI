3fec5501906e3c8b5440b405a8f0cb1ea50c1f49
Stefan Chen <zchen158@my.bcit.ca>
2022-07-06 20:28:36 -0700

    Add filter to search input and show matched results only

--- react_material/src/components/Pokedex.js
+++ react_material/src/components/Pokedex.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import {
   AppBar,
   Toolbar,
@@ -9,64 +9,71 @@ import {
   CardContent,
   CircularProgress,
   Typography,
-  TextField
+  TextField,
 } from "@material-ui/core";
 import { fade, makeStyles } from "@material-ui/core/styles";
-import SearchIcon from '@material-ui/icons/Search'
-import { toFirstCharUpperCase } from './constants'
+import SearchIcon from "@material-ui/icons/Search";
+import { toFirstCharUpperCase } from "./constants";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   pokedexContainer: {
     paddingTop: "20px",
     paddingLeft: "50px",
     paddingRight: "50px",
   },
   CardMedia: {
-    margin: "auto"
+    margin: "auto",
   },
   CardContent: {
-    textAlign: 'center'
+    textAlign: "center",
   },
   searchContainer: {
-    display: 'flex',
+    display: "flex",
     backgroundColor: fade(theme.palette.common.white, 0.15),
     paddingLeft: "20px",
     paddingRight: "20px",
-    marginTop: '5px',
-    marginBottom: '5px'
+    marginTop: "5px",
+    marginBottom: "5px",
   },
   searchIcon: {
-    alignSelf: 'flex-end',
-    marginBottom: '5px'
+    alignSelf: "flex-end",
+    marginBottom: "5px",
   },
   searchInput: {
-    width: '200px',
-    margin: '5px'
-  }
+    width: "200px",
+    margin: "5px",
+  },
 }));
 
 function Pokedex(props) {
-  const {history} = props
+  const { history } = props;
   const classes = useStyles();
   const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon`).then(function (res) {
       // 'data' comes natively inside response for any api request
-      const {data} = res
-      const {results} = data
-      const newPokemonData = {}
+      const { data } = res;
+      const { results } = data;
+      const newPokemonData = {};
       // pokemon 是一个object, index 是results array 的 index, 从0开始
       results.forEach((pokemon, index) => {
         newPokemonData[index + 1] = {
           id: index + 1,
           name: pokemon.name,
-          sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
-        }
-      })
-      setPokemonData(newPokemonData)
-    })
-  }, [])
+          sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`,
+        };
+      });
+      setPokemonData(newPokemonData);
+    });
+  }, []);
 
   const getPokemonCard = (pokemonId) => {
     const { id, name, sprite } = pokemonData[pokemonId];
@@ -93,15 +100,22 @@ function Pokedex(props) {
         <Toolbar>
           <div className={classes.searchContainer}>
             <SearchIcon className={classes.searchIcon} />
-            <TextField className={classes.searchInput}/>
+            <TextField
+              onChange={handleSearchChange}
+              className={classes.searchInput}
+              label="Pokemon"
+              variant="standard"
+            />
           </div>
-          </Toolbar>
+        </Toolbar>
       </AppBar>
       {/* 如果pokemonData存在，就显示grid; 如果不存在，就显示loading的图标 */}
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
-          {Object.keys(pokemonData).map((pokemonId) =>
-            getPokemonCard(pokemonId)
+          {Object.keys(pokemonData).map(
+            (pokemonId) =>
+              pokemonData[pokemonId].name.includes(filter) &&
+              getPokemonCard(pokemonId)
           )}
         </Grid>
       ) : (
